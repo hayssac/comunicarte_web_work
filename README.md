10b4224cb2ea58fee626cfcb7e02b43b6f16752e
Hayssa Hackradt <hayssa.hscosta@gmail.com>
2017-07-26 18:59:14 -0300

    Submissão do projeto no GitHub a fim de portfólio

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# comunicarte_web_work
-Template para landing page da empresa Comunicarte Web, feito em 2016
+# Projeto 
+
+### Contexto
+
+- Projeto realizado em período de estágio na empresa Comunicarte Web, em 2016, startup que era
+incubada no ITNC (Incubadora Tecnológica do Campus Natal-Central) no IFRN.
+
+### Cliente
+- Suzyanne Oliveira, ex-supervisora de estágio
+
+### Mentora
+- Nayara Rocha, UX/UI e front-end dev na Comunicarte Web
+
+# Tecnologias
+
+- Esta versão do template é estática, contendo apenas HTML, CSS (Bootstrap como framework) e 
+JavaScript (jQuery)
+
+
---
+++ js/main.js
@@ -0,0 +1,90 @@
+jQuery(function($) {
+
+	//Preloader
+	var preloader = $('.preloader');
+	$(window).load(function(){
+		preloader.remove();
+	});
+
+	//#main-slider
+	var slideHeight = $(window).height();
+	$('#home-slider .item').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('#home-slider .item').css('height',slideHeight);
+	});
+	
+	//Scroll Menu and Closing sidear
+	$(window).on('scroll', function(){
+		if( $(window).scrollTop()>slideHeight ){
+			$('.main-nav').addClass('navbar-fixed-top');
+		} else {
+			$('.main-nav').removeClass('navbar-fixed-top');
+		}
+	});
+	$(window).on('scroll', function(){
+		if($(window).scrollTop()>slideHeight) {
+			$("#sidebar-wrapper-right").removeClass("active-right");
+		}
+	});
+
+	
+	// Navigation Scroll
+	$(window).scroll(function(event) {
+		Scroll();
+	});
+
+	$('.navbar-collapse ul li a').on('click', function() {  
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+		return false;
+	});
+
+	// User define function
+	function Scroll() {
+		var contentTop      =   [];
+		var contentBottom   =   [];
+		var winTop      =   $(window).scrollTop();
+		var rangeTop    =   200;
+		var rangeBottom =   500;
+		$('.navbar-collapse').find('.scroll a').each(function(){
+			contentTop.push( $( $(this).attr('href') ).offset().top);
+			contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
+		})
+		$.each( contentTop, function(i){
+			if ( winTop > contentTop[i] - rangeTop ){
+				$('.navbar-collapse li.scroll')
+				.removeClass('active')
+				.eq(i).addClass('active');			
+			}
+		})
+	};
+
+	$('#tohash').on('click', function(){
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+		return false;
+	});
+
+	//Sidebar
+	$("#menu-right-open").click(function(e) {
+	  e.preventDefault();
+	  $("#sidebar-wrapper-right").addClass("active-right");
+	});
+
+	$("#menu-right-close").click(function(e) {
+		e.preventDefault();
+		$("#sidebar-wrapper-right").removeClass("active-right");
+	});
+
+	//Click li of ~sidebar~ and close
+	$(".scroll").click(function(e) {
+		e.preventDefault;
+		$("#sidebar-wrapper-right").removeClass("active-right");
+	});
+
+	//Initiat WOW JS
+	new WOW().init();
+	//smoothScroll
+	smoothScroll.init();
+
+});
+
